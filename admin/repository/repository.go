c049e2eba1eb2c1d5475e4205d13527f2bf17295
SumbalMir786 <sumbal.alphasquad@gmail.com>
2024-05-31 15:28:27 +0500

    code updated

--- admin/repository/repository.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-type AdminRepository interface {
-	// Define admin repository methods here
-}
-
-type DBAdminRepository struct {
-	// Implement admin repository using database
-}
